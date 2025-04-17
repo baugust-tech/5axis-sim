e4498b8b5cd058b4f06750c2f4883f6383f83483
baugust-tech <baugust.ba@gmail.com>
2025-04-17 12:49:07 -0500

    Add files via upload

---
+++ SiemensNCViewer.jsx
@@ -0,0 +1,4 @@
+
+export default function SiemensNCViewer() {
+  return <div style={{ padding: 20, fontFamily: 'monospace' }}>[SiemensNCViewer.jsx placeholder loaded successfully]</div>
+}
---
+++ main.jsx
@@ -0,0 +1,10 @@
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import SiemensNCViewer from './SiemensNCViewer'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <SiemensNCViewer />
+  </React.StrictMode>
+)
